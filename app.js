c323b4eadb3266e79542926bc944834c8ede2d33
Lautaro Exequiel Tapia <lautaro.online@gmail.com>
2025-03-10 22:43:13 -0300

    fix:app.js , se corrigio el error que no dejaba conectarse a la base de datos

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
-ECHO est  activado.
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://Grupo-13:grupo13@cursadanodejs.ls9ii.mongodb.net/Node-js')
+.then(()=> console.log('Conexion exitosa a MongodDb'))
+.catch(error=>console.error('Error al conectar a MongoDB'));
